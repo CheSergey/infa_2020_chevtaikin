718841c9af1dff20fe2a4c33a5379ddca81158b6
CheSergey <sergeychevtaykin@gmail.com>
2021-06-20 23:13:09 +0200

    First realese of picure with blue sky

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # the beginning of training for drawing
 
-def house_build(x, y, z):
+import graphics as gr
+
+window = gr.GraphWin("Черчу черчу", 1200, 800)
+
+
+def circle_drw(coord_x, cord_y, radius, line_width, outline_colour, fill_colour):
+    circle = gr.Circle(gr.Point(coord_x, cord_y), radius)
+    circle.setOutline(outline_colour)
+    circle.setWidth(line_width)
+    circle.setFill(fill_colour)
+    circle.draw(window)
     return ()
 
-x = input("What'your name?")
-print(x)
 
-print('House has been built')
+def rectangle_drw(start_point_x, start_point_y, end_point_x, end_point_y, line_width, outline_color, fill_color):
+    rectangle = gr.Rectangle(gr.Point(start_point_x, start_point_y), (gr.Point(end_point_x, end_point_y)))
+    rectangle.setOutline(outline_color)
+    rectangle.setWidth(line_width)
+    rectangle.setFill(fill_color)
+    rectangle.draw(window)
+    return ()
 
-if __name__ == "__main__":
-    house_build(100, 200, 300)
+
+rectangle_drw(0, 0, 1200, 400, 2, "blue", "blue")
+
+window.getMouse()
